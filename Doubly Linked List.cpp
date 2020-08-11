2fcfaf359186c5d4e1de1d9d2ef1d53e5021efc1
Raphael Gutierrez <urd1_17@yahoo.com>
2020-08-12 00:17:35 +0800

    Update Doubly Linked List.cpp

--- Doubly Linked List.cpp
+++ Doubly Linked List.cpp
@@ -22,7 +22,8 @@ struct node *createNode(){
 }
 
 void display();
-struct node *find();
+struct node *findByPos();
+struct node *findByValue();
 
 
 void insertBeginning(){
@@ -57,6 +58,122 @@ void insertEnd(){
 	}
 }
 
+int insertSpecific(){
+	struct node *newnode, *old;
+	int pos;
+	
+	printf("\nInsert node at position: ");
+	scanf("%d", &pos);
+	
+	if(head == NULL){
+		printf("\nList is empty.");
+	}
+	else{
+		old = head;
+		for(int i = 0; i < pos; i++){
+			if(old->right == NULL){
+				printf("\nPosition out of bounds.\n");
+				return 0;
+			}
+			old = old->right;
+			
+		}
+	}
+	
+	printf("\nFound:%p\t%d(%p)\t%p\n", old->left, old->value, old, old->right);
+	
+	newnode = createNode();
+	
+	if(old == NULL){
+		printf("\nInsertion is invalid.");
+	}
+	else{
+		if(old == head)
+			head = newnode;
+		
+		if(old->left != NULL){
+			newnode->left = old->left;
+			old->left->right = newnode;
+			old->left = newnode;
+		}
+		
+		newnode->right = old;
+	}
+}
+
+void deleteBeginning(){
+	struct node *current;
+	
+	if(head == NULL){
+		printf("\nList is empty.");
+	}
+	else{
+		current = head;
+		head = current->right;
+		head->left = NULL;
+		free(current);
+	}
+}
+
+void deleteEnd(){
+	struct node *current;
+	
+	if(head == NULL){
+		printf("\nList is empty.");
+	}
+	else{
+		current = tail;
+		tail = current->left;
+		tail->right = NULL;
+		free(current);
+	}
+}
+
+int deleteSpecific(){
+	struct node *current;
+	int pos;
+	
+	printf("\nDelete node at position: ");
+	scanf("%d", &pos);
+	
+	if(head == NULL){
+		printf("\nList is empty.");
+	}
+	else{
+		current = head;
+		for(int i = 0; i < pos; i++){
+			if(current->right == NULL){
+				printf("\nPosition out of bounds.");
+				return 0;
+			}
+			current = current->right;
+		}
+	}
+	
+	if(current == NULL){
+		printf("\nDeletion is invalid.\n");
+	}
+	else{
+		if(current == head){
+			head = current->right;
+			current->right->left = NULL;
+			free(current);
+			return 0;
+		}
+		if(current == tail){
+			tail = current->left;
+			current->left->right = NULL;
+			free(current);
+			return 0;
+		}
+
+		current->left->right = current->right;
+		current->right->left = current->left;
+		free(current);
+		return 0;	
+	}
+}
+
 int main(){
 	struct node *current, *temp;
 	
@@ -82,17 +199,47 @@ int main(){
 	
 	insertBeginning();
 	insertEnd();
-	struct node *found = find();
-	if(found != 0)
-		printf("\nFound:%p\t%d(%p)\t%p\n", found->left, found->value, found, found->right);
-	else
-		printf("Not found.\n");
+//	struct node *foundValue = findByValue();
+//	if(foundValue != 0)
+//		printf("\nFound:%p\t%d(%p)\t%p\n", foundValue->left, foundValue->value, foundValue, foundValue->right);
+//	else
+//		printf("Not found.\n");
+	display();
+	deleteSpecific();
+	
 	
 	display();
+	struct node *foundPos = findByPos();
+	if(foundPos != 0)
+		printf("\n\nFound:%p\t%d(%p)\t%p\n", foundPos->left, foundPos->value, foundPos, foundPos->right);
+	
 }
 
 
-struct node *find(){
+struct node *findByPos(){
+	struct node *t;
+	int pos;
+	
+	printf("\nEnter position to find: ");
+	scanf("%d", &pos);
+	
+	if(head == NULL){
+		printf("\nList is empty.");
+	}
+	else{
+		t = head;
+		for(int i = 0; i < pos; i++){
+			if(t->right == NULL){
+				printf("\nPosition out of bounds.\n");
+				return 0;
+			}
+			t = t->right;
+		}
+		return t;
+	}
+}
+
+struct node *findByValue(){
 	struct node *t;
 	int data;
 	
