e03969c4c79ad28283c30ba9b4e6c76f050b6f9b
Raphael Gutierrez <urd1_17@yahoo.com>
2020-08-14 00:01:59 +0800

    Add files via upload

--- Linked List.cpp
+++ Linked List.cpp
@@ -1,57 +1,259 @@
-// https://www.youtube.com/watch?v=4IrUAqYKjIA
-
 #include <stdio.h>
-#include <stdlib.h>
+#include <malloc.h>
 
-typedef struct node{
-	int num;
-	struct node *next;
-}node;
+struct node{
+	int value;
+	struct node *nextaddress;
+};
 
-int main(void){
-	// make an empty list of nodes
-	node *list = NULL;
+
+struct node *head, *tail;
+
+
+struct node *createNode(){
+	struct node *newnode;
+	newnode = (struct node*) malloc(sizeof(struct node));
+	newnode->nextaddress = NULL;
 	
-	// allocate a memory for first node
-	node *n = (struct node*) malloc(sizeof(node));
-	// make sure n will return a memory
-	if(n == NULL)
-		return 1;
+	printf("Enter value: ");
+	scanf("%d", &newnode->value);
+	
+	return(newnode);
+}
+
+
+void insertBeginning(){
+	struct node *newnode;
+	newnode = createNode();
+	
+	if(head == NULL){
+		head = newnode;
+		tail = newnode;
+	}
+	else{
+		newnode->nextaddress = head;
+		head = newnode;
+	}
+}
+
+void insertEnd(){
+	struct node *newnode;
+	newnode = createNode();
+	
+	if(head == NULL){
+		head = newnode;
+		tail = newnode;
+	}
+	else{
+		tail->nextaddress = newnode;
+		tail = newnode;
+	}
+}
+
+int insertSpecific(){
+	struct node *newnode, *temp, *beforetemp;
+	int pos;
+	
+	newnode = createNode();
+	
+	printf("\nInsert at position: ");
+	scanf("%d", &pos);
+	
+	if(head == NULL){
+		head = newnode;
+		tail = newnode;
+	}
+	else{
+		temp = head;
+		for(int i = 0; i < pos; i++){
+			beforetemp = temp;
+			
+			if(temp->nextaddress == NULL){
+				printf("\nInsertion out of bounds.\n");
+				return 0;
+			}
+			
+			temp = temp->nextaddress;
+		}
 		
-	// assign a number, next address to NULL
-	n->num = 1;
-	n->next = NULL;
-	
-	// pass the current node to the list
-	list = n;
-	
-	
-	// creation of memory for another node
-	n = (node*) malloc(sizeof(node));
-	
-	if(n == NULL)
-		return 1;
+		if(temp == head){
+			newnode->nextaddress = temp;
+			head = newnode;
+			return 0;
+		}
 		
-	n->num = 3;
-	n->next = NULL;
+		newnode->nextaddress = temp;
+		beforetemp->nextaddress = newnode;
+	}
+}
+
+void deleteBeginning(){
+	struct node *temp;
 	
-	// nests the list using next address
-	list->next = n;
+	if(head == NULL)
+		printf("\nDeletion out of bounds.\n");
+	if(head == tail){
+		head = NULL;
+		tail = NULL;
+	}
+	else{
+		temp = head;
+		head = temp->nextaddress;
+		free(temp);
+	}
+}
+
+void deleteEnd(){
+	struct node *temp;
 	
+	if(head == NULL)
+		printf("\nDeletion out of bounds.\n");
+	if(head == tail){
+		head = NULL;
+		tail = NULL;
+	}
+	else{
+		temp = head;
+		while(temp->nextaddress != tail)
+			temp = temp->nextaddress;
+			
+		free(tail);
+		tail = temp;
+		temp->nextaddress = NULL;
+	}
+}
+
+int deleteSpecific(){
+	struct node *temp, *beforetemp;
+	int pos;
 	
-	n = (node*) malloc(sizeof(node));
+	printf("\nDelete position: ");
+	scanf("%d", &pos);
 	
-	if(n == NULL)
-		return 1;
+	if(head == NULL)
+		printf("\nDeletion invalid.\n");
+	else{
+		temp = head;
+		for(int i = 0; i < pos; i++){
+			beforetemp = temp;
+			if(temp->nextaddress == NULL){
+				printf("\nPosition out of bounds.\n");
+				return 0;
+			}
+			temp = temp->nextaddress;
+		}
 		
-	n->num = 5;
-	n->next = NULL;
-	list->next->next = n;
+		if(temp == head){
+			head = temp->nextaddress;
+			free(temp);
+			return 0;
+		}
+		if(temp == tail){
+			tail = beforetemp;
+			beforetemp->nextaddress = NULL;
+			free(temp);
+			return 0;
+		}
+		
+		beforetemp->nextaddress = temp->nextaddress;
+		free(temp);
+	}
+}
+
+int findByPos(){
+	struct node *temp;
+	int pos;
 	
-	// printing the list
-	for(node *tmp = list; tmp != NULL; tmp = tmp->next){
-		printf("%d ", tmp->num);
+	printf("\nEnter position to find: ");
+	scanf("%d", &pos);
+	
+	if(head == NULL){
+		printf("\nList is empty.\n");
+		return 0;
 	}
 	
-	free(n);
+	temp = head;
+	for(int i = 0; i < pos; i++){
+		if(temp->nextaddress == NULL){
+			printf("\nPosition out of bounds.\n");
+			return 0;
+		}
+		temp = temp->nextaddress;
+	}
+	
+	printf("\nFound %d(%p) %p\n", temp->value, temp, temp->nextaddress);
+}
+
+
+int findByValue(){
+	struct node *temp;
+	int val, pos = 0;
+	
+	printf("\nEnter value to find: ");
+	scanf("%d", &val);
+	
+	if(head == NULL){
+		printf("\nList is empty.\n");
+		return 0;
+	}
+	
+	temp = head;
+	while(temp != NULL){
+		if(temp->value == val){
+			printf("\nFound at position %d: %d(%p) %p\n", pos, temp->value, temp, temp->nextaddress);
+			return 0;
+		}
+		
+		pos++;
+		temp = temp->nextaddress;
+	}
+	
+	printf("\nNot found.\n");
+}
+
+
+int display(){
+	struct node *temp;
+	
+	if(head == NULL){
+		printf("\nList is empty.\n");
+		return 0;
+	}
+	
+	printf("\nHead: %d, Tail: %d\n", head->value, tail->value);
+	
+	printf("\nValue(Node)\t\tNext Node\n");
+	
+	temp = head;
+	while(temp != NULL){
+		printf("%d(%p)\t%p\n", temp->value, temp, temp->nextaddress);
+		temp = temp->nextaddress;
+	}
+}
+
+int main(){
+	struct node *newnode, *temp;
+	
+	for(int i = 0; i < 4; i++){
+		newnode = createNode();
+		
+		if(head == NULL){
+			head = newnode;
+			tail = newnode;
+		}
+		else{
+			temp = head;
+			
+			while(temp->nextaddress != NULL)
+				temp = temp->nextaddress;
+			
+			temp->nextaddress = newnode;
+			tail = newnode;
+		}
+	}
+	
+	display();
+	//insertSpecific();
+	findByValue();
+	display();
 }
