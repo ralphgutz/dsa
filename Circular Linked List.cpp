2669dabe61f5cf428b208f604420bf2a33f73ccc
Raphael Gutierrez <urd1_17@yahoo.com>
2020-08-18 23:28:48 +0800

    Update Circular Linked List.cpp

--- Circular Linked List.cpp
+++ Circular Linked List.cpp
@@ -48,7 +48,7 @@ void insertEnd(int data){
 	tail->nextaddress = head;
 }
 
-void insertSpecific(int data, int pos){
+int insertSpecific(int data, int pos){
 	struct node *newnode, *temp;
 	newnode = createNode();
 	
@@ -86,6 +86,74 @@ void insertSpecific(int data, int pos){
 	}
 }
 
+void deleteBeginning(){
+	struct node *temp;
+	
+	if(head == NULL && tail == NULL){
+		printf("\nList is empty.\n");
+	}
+	else{
+		temp = head;
+		head = head->nextaddress;
+		free(temp);
+		tail->nextaddress = head;
+	}
+}
+
+void deleteEnd(){
+	struct node *temp, *beforetail;
+	
+	if(head == NULL && tail == NULL){
+		printf("\nList is empty.\n");
+	}
+	else{
+		beforetail = head;
+		while(beforetail->nextaddress != tail){  // gets the node before tail
+			beforetail = beforetail->nextaddress;
+		}
+		
+		beforetail->nextaddress = tail->nextaddress;
+		temp = tail;
+		free(temp);
+		tail = beforetail;
+	}
+}
+
+void deleteSpecific(int pos){
+	struct node *temp, *todelete;
+	
+	if(head == NULL && tail == NULL){
+		printf("\nList is empty.\n");
+	}
+	else{
+		temp = head;
+		for(int i = 0; i < pos-1; i++){
+			temp = temp->nextaddress;
+		}  // temp will be the node before todelete
+
+		if(temp == head && pos == 0){
+			printf("\nDeleting %d (%p)\n", temp->value, temp);
+			tail->nextaddress = temp->nextaddress;
+			head = temp->nextaddress;
+			free(temp);
+		}
+		else{
+			printf("\nDeleting %d (%p)\n", temp->nextaddress->value, temp->nextaddress);
+			todelete = temp->nextaddress;
+			
+			if(temp->nextaddress == tail){  // deleting tail
+				tail = temp;
+			}
+			else if(temp == tail){  // deleting head after 1 revolution
+				head = head->nextaddress;
+			}
+			
+			temp->nextaddress = todelete->nextaddress;  // assigning temp's nextaddress to todelete's nextaddress
+			free(todelete);
+		}
+	}
+}
+
 void display(){
 	struct node *temp;
 	
@@ -99,7 +167,7 @@ void display(){
 		printf("\nValue(Node)\t\tNext Node\n");
 		
 		temp = head;
-		while(temp->nextaddress != head){
+		while(temp->nextaddress != head){  // so it won't display forever
 			printf("%d (%p)\t%p\n", temp->value, temp, temp->nextaddress);
 			temp = temp->nextaddress;
 		}
@@ -115,3 +183,9 @@ int main(){
 	insertSpecific(69, 8);
 	display();
 }
+
+/**  TO GET THE SPECIFIC NODE IN TEMP:
+	for(int i = 0; i < pos; i++){
+		temp = temp->nextaddress;
+	}
+**/
