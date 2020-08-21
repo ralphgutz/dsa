db52b83714b904affad0a906a1351dd4f1970d3e
Raphael Gutierrez <urd1_17@yahoo.com>
2020-08-21 23:41:59 +0800

    Update Bubble Sort.cpp

--- Sorting/Bubble Sort.cpp
+++ Sorting/Bubble Sort.cpp
@@ -5,9 +5,9 @@ int bubbleSort(int arr[], int n){
 	for(int i = 0; i < n-1; i++){
 		printf("PASS %d (i=%d)\n", i+1, i);
 		
-		for(int j = 0; j < n-1-i; j++){
+		for(int j = 0; j < n-1-i; j++){  // when i increases, no. of comparison decreases (so n-1-i)
 			
-			if(arr[j] > arr[j+1]){
+			if(arr[j] > arr[j+1]){  // swap positions if arr[0]=16 > arr[1]=14
 				int temp = arr[j];
 				arr[j] = arr[j+1];
 				arr[j+1] = temp;
@@ -25,7 +25,7 @@ int bubbleSort(int arr[], int n){
 int bubbleSortOptimized(int arr[], int n){
 	
 	for(int i = 0; i < n-1; i++){
-		int flag = 0;
+		int flag = 0;  // flag to end
 		
 		printf("PASS %d (i=%d)\n", i+1, i);
 		
@@ -36,17 +36,18 @@ int bubbleSortOptimized(int arr[], int n){
 				arr[j] = arr[j+1];
 				arr[j+1] = temp;
 				
+				// displaying only
 				for(int i = 0; i < n; i++)
 					printf(" %d", arr[i]);
 				
 				printf("\n");
 				
-				flag = 1;
+				flag = 1;  // flag=1 if swapping occured
 			}
 		}
 		printf("\n");
 		
-		if(flag == 0){
+		if(flag == 0){  // if no swapping occured, break
 			printf(" No swapping needed.\n");
 			break;
 		}
@@ -56,13 +57,15 @@ int bubbleSortOptimized(int arr[], int n){
 int main(){
 	int arr[5] = {16, 14, 5, 6, 8};
 	int arr2[5] = {16, 14, 5, 6, 8};
+	
 	printf("UNOPTIMIZED ==========\n\n");
 	bubbleSort(arr, 5);
+	
 	printf("\nOPTIMIZED ============\n\n");
 	bubbleSortOptimized(arr2, 5);
 }
 
 /**
-	Maximum passes = n-1
+	Maximum passes: n-1
 	With optimized bubble sort, the loop with no passing needed ends
 **/
